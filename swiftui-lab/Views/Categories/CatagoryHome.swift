66b8b26953262b10b82c70634bf60b2bb2044c2b
Erik R. Yverling <erik.r@yverling.se>
2024-01-18 22:58:11 +0100

    SwiftUI tutorial - Working with UI Controls

---
+++ swiftui-lab/Views/Categories/CatagoryHome.swift
@@ -0,0 +1,30 @@
+import SwiftUI
+
+struct CategoryHome: View {
+    @Environment(ModelData.self) var modelData
+    
+    var body: some View {
+        NavigationSplitView {
+            List {
+                modelData.features[0].image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
+                
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    CategoryRow(categoryName: key, items: modelData.categories[key]!)
+                }
+            }
+            .navigationTitle("Featured")
+        } detail: {
+            Text("Select a Landmark")
+        }
+    }
+}
+
+#Preview {
+    CategoryHome()
+        .environment(ModelData())
+}
--- swiftui-lab/Views/ContentView.swift
+++ swiftui-lab/Views/ContentView.swift
@@ -1,8 +1,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection: Tab = .featured
+    
+    enum Tab {
+        case featured
+        case list
+    }
+    
     var body: some View {
-        LandmarkList()
+        TabView(selection: $selection) {
+            CategoryHome()
+                .tabItem {
+                    Label("Featured", systemImage: "star")
+                }
+            
+            
+            LandmarkList()
+                .tabItem {
+                    Label("List", systemImage: "list.bullet")
+                }
+        }
         
     }
 }
