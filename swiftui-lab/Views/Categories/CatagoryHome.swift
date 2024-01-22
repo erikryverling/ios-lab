4783270e444acc91dc134ec44b3304fe6eea9e15
Erik R. Yverling <erik.r@yverling.se>
2024-01-22 22:20:20 +0100

    Swift UI tutorial - Working with UI controls

--- swiftui-lab/Views/Categories/CatagoryHome.swift
+++ swiftui-lab/Views/Categories/CatagoryHome.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 struct CategoryHome: View {
     @Environment(ModelData.self) var modelData
+    @State private var showingProfile = false
     
     var body: some View {
         NavigationSplitView {
@@ -17,7 +18,20 @@ struct CategoryHome: View {
                     CategoryRow(categoryName: key, items: modelData.categories[key]!)
                 }
             }
+            .listStyle(.inset)
             .navigationTitle("Featured")
+            .toolbar {
+                Button {
+                    showingProfile.toggle()
+                } label: {
+                    Label("User Profile", systemImage: "person.crop.circle")
+                }
+            }
+            .sheet(isPresented: $showingProfile) {
+                ProfileHost()
+                    .environment(modelData)
+            }
+            
         } detail: {
             Text("Select a Landmark")
         }
